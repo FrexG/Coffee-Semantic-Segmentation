ec9afd5743330c40558fe5e0508d115d108ee5ce
= <fraolgela@gmail.com>
2022-09-16 18:59:11 +0300

    gitignore

--- models/train.py
+++ models/train.py
@@ -94,21 +94,21 @@ class TrainModel:
                     eval_progress_bar.set_description(f"Evaluation")
                     eval_progress_bar.set_postfix(avg_dice_val_loss=round(total_test_dice_loss.data.item() / test_step,4),avg_val_dice_coeff=round(1-(total_test_dice_loss.data.item() / test_step),4))
                     eval_progress_bar.update()
-        # save history
-        history['epochs'].append(epoch)
-        history['training_dice_loss'].append(round(total_train_dice_loss.data.item() / train_step,4))
-        history['test_dice_loss'].append(round(total_test_dice_loss.data.item() / test_step,4))
+            # save history
+            history['epochs'].append(epoch)
+            history['training_dice_loss'].append(round(total_train_dice_loss.data.item() / train_step,4))
+            history['test_dice_loss'].append(round(total_test_dice_loss.data.item() / test_step,4))
 
-        # save model every 5 epochs
-        if (epoch + 1) % 5 == 0:
-            date_postfix = datetime.now().strftime("%Y-%m-%d")
-            model_name = f'unet_coffee_{date_postfix}.pth'
-            save_path = "../weights"
+            # save model every 5 epochs
+            if (epoch + 1) % 5 == 0:
+                date_postfix = datetime.now().strftime("%Y-%m-%d")
+                model_name = f'unet_coffee_{date_postfix}.pth'
+                save_path = "../weights"
 
-            if not os.path.exists(save_path):
-                os.mkdir(save_path)
-            else:
-                print(f"[INFO] Saving model to {os.path.join(save_path,model_name)}")
-                torch.save(model.state_dict(),os.path.join(save_path,model_name))
+                if not os.path.exists(save_path):
+                    os.mkdir(save_path)
+                else:
+                    print(f"[INFO] Saving model to {os.path.join(save_path,model_name)}")
+                    torch.save(model.state_dict(),os.path.join(save_path,model_name))
                 
         return model,history
