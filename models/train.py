a1d4b34f775a8ad51b05a6270b4bacfbcfdab08c
= <fraolgela@gmail.com>
2022-09-16 12:48:43 +0300

    Modified training class added weights saving

--- models/train.py
+++ models/train.py
@@ -1,87 +1,114 @@
 from tqdm import tqdm
+import os
 import torch
-
-def train(*,model,criterion,opt,train_loader,test_loader,train_data_len,test_data_len,device,batch_size,num_epoch:int=1):
-    """ A Function to train a model
-        returns -> trained model
-
+from torch.utils.data import DataLoader
+from datetime import datetime
+class TrainModel:
+    """ Model Training class
+        returns -> None
         `Parameters`
         ----------
         `model`: torch model
-        `criterion`: The loss function
-        `opt`: Gradient optimization function of type 'torch.optim.'
         `train_loader`: torch DataLoader instance for the training data
         `test_loader`: torch DataLoader instance for the testing data
-        `num_epoch`: The number of epoch for training
-
+        `train_data_len`: length of the training data
+        `test_data_len`: length of the testing data
+        `device`: acceleration device (cpu,cuda or mps)
     """
-    train_step = train_data_len // batch_size
-    test_step = test_data_len // batch_size
-    # Start training loop
-    history = {'training_dice_loss':[],'test_dice_loss':[],'epochs':[]}
-    # Model to device
-    model = model.to(device)
-    for epoch in range(num_epoch):
-        # set the model to train mode
-
-        model.train()
-        # initialize total train and test loss
-        total_train_loss = 0
-        total_train_dice_loss = 0
-        total_test_loss = 0
-        total_test_dice_loss = 0
-
-        progress_bar = tqdm(enumerate(train_loader),total=len(train_loader),desc='Training')
-        for i,(images,masks) in progress_bar:
-            # Move the images and masks to device
-            images = images.to(device)
-            masks = masks.to(device)
-            # Perform forward pass and calculate the loss
-            preds = model(images)
+    def __init__(self,model,test_loader:DataLoader,train_loader:DataLoader,train_data_len:int,test_data_len:int,device:torch.device=torch.device("cpu")) -> None:
+        """ Initialization """
         
-            loss = criterion(preds,masks)
-            dice_loss = criterion(preds,masks)
-            
-            # zero grand and back-propagate
-            opt.zero_grad()
-            dice_loss.backward()
-            opt.step()
-            total_train_loss += loss
-            total_train_dice_loss += dice_loss
+        self.model = model
+        self.test_loader = test_loader
+        self.train_loader = train_loader
+        self.train_data_len = train_data_len
+        self.test_data_len = test_data_len
+        self.device = device
 
-            progress_bar.set_description(f"Epoch {epoch+1}/{num_epoch}")
-            progress_bar.set_postfix(loss=round(total_train_loss.data.item()/(i+1),4),dice_loss=round(total_train_dice_loss.data.item()/(i+1),4),dice_coeff=round(1-(total_train_dice_loss.data.item()/(i+1)),4))
-            progress_bar.update()
-        
-            # Evaluate the model on test set
-        with torch.no_grad():
-            #set the model to evaluation mode
-            model.eval()
-            eval_progress_bar = tqdm(enumerate(test_loader),total=len(test_loader),desc='Evaluation')
-            for i,(images,masks) in eval_progress_bar:
+    def train(self,*,criterion,optimizer,batch_size,num_epoch:int=1):
+        """ A Function to train a model
+            returns -> trained model
+
+            `Parameters`
+            ----------
+            `criterion`: The loss function
+            `opt`: Gradient optimization function of type 'torch.optim.'
+            `num_epoch`: The number of epoch for training
+
+        """
+        train_step = self.train_data_len // batch_size
+        test_step = self.test_data_len // batch_size
+        # Start training loop
+        history = {'training_dice_loss':[],'test_dice_loss':[],'epochs':[]}
+        # Model to device
+        model = self.model.to(self.device)
+        for epoch in range(num_epoch):
+            # set the model to train mode
+
+            model.train()
+            # initialize total train and test loss
+            total_train_loss = 0
+            total_train_dice_loss = 0
+            total_test_loss = 0
+            total_test_dice_loss = 0
+
+            progress_bar = tqdm(enumerate(self.train_loader),total=len(self.train_loader),desc='Training')
+            for i,(images,masks) in progress_bar:
                 # Move the images and masks to device
-                images = images.to(device)
-                masks = masks.to(device)
-                    # Perform forward pass and calculate the loss
+                images = images.to(self.device)
+                masks = masks.to(self.device)
+                # Perform forward pass and calculate the loss
                 preds = model(images)
-                
+            
                 loss = criterion(preds,masks)
                 dice_loss = criterion(preds,masks)
-                total_test_loss += loss
-                total_test_dice_loss += dice_loss
+                
+                # zero grand and back-propagate
+                optimizer.zero_grad()
+                dice_loss.backward()
+                optimizer.step()
+                total_train_loss += loss
+                total_train_dice_loss += dice_loss
 
-                eval_progress_bar.set_description(f"Evaluation")
-                eval_progress_bar.set_postfix(avg_loss=round(total_test_loss.data.item() / test_step,4),avg_dice_val_loss=round(total_test_dice_loss.data.item() / test_step,4),avg_val_dice_coeff=round(1-(total_test_dice_loss.data.item() / test_step),4))
-                eval_progress_bar.update()
-    # save history
-    history['epochs'].append(epoch)
-    history['training_dice_loss'].append(round(total_train_dice_loss.data.item() / train_step,4))
-    history['test_dice_loss'].append(round(total_test_dice_loss.data.item() / test_step,4))
+                progress_bar.set_description(f"Epoch {epoch+1}/{num_epoch}")
+                progress_bar.set_postfix(dice_loss=round(total_train_dice_loss.data.item()/(i+1),4),dice_coeff=round(1-(total_train_dice_loss.data.item()/(i+1)),4))
+                progress_bar.update()
+            
+                # Evaluate the model on test set
+            with torch.no_grad():
+                #set the model to evaluation mode
+                model.eval()
+                eval_progress_bar = tqdm(enumerate(self.test_loader),total=len(self.test_loader),desc='Evaluation')
+                for i,(images,masks) in eval_progress_bar:
+                    # Move the images and masks to device
+                    images = images.to(self.device)
+                    masks = masks.to(self.device)
+                        # Perform forward pass and calculate the loss
+                    preds = model(images)
+                    
+                    loss = criterion(preds,masks)
+                    dice_loss = criterion(preds,masks)
+                    total_test_loss += loss
+                    total_test_dice_loss += dice_loss
 
-    """ # save model every 10 epochs
-    if (epoch + 1) % 10 == 0:
-        model_path = 'unet_torch_model.pth'
-        print(f"[INFO] Saving model to {model_path}")
-        torch.save(model.state_dict(),model_path) """
+                    eval_progress_bar.set_description(f"Evaluation")
+                    eval_progress_bar.set_postfix(avg_dice_val_loss=round(total_test_dice_loss.data.item() / test_step,4),avg_val_dice_coeff=round(1-(total_test_dice_loss.data.item() / test_step),4))
+                    eval_progress_bar.update()
+        # save history
+        history['epochs'].append(epoch)
+        history['training_dice_loss'].append(round(total_train_dice_loss.data.item() / train_step,4))
+        history['test_dice_loss'].append(round(total_test_dice_loss.data.item() / test_step,4))
 
-    return model,history
+        # save model every 5 epochs
+        if (epoch + 1) % 5 == 0:
+            date_postfix = datetime.now().strftime("%Y-%m-%d")
+            model_name = f'unet_coffee_{date_postfix}.pth'
+            save_path = "../weights"
+
+            if not os.path.exists(save_path):
+                os.mkdir(save_path)
+            else:
+                print(f"[INFO] Saving model to {os.path.join(save_path,model_name)}")
+                torch.save(model.state_dict(),os.path.join(save_path,model_name))
+                
+        return model,history
